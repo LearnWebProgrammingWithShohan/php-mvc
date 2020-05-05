6d9cef48892b29f3fbf65311e3d8a8b4acd8bf8c
Arup <arupctg@gmail.com>
2020-05-04 22:13:54 -0800

    Student Model

---
+++ src/Arup-paul/app/model/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Student {
+
+    public $name;
+    public $age;
+    public $roll;
+
+    public function __construct($name,$age,$roll){
+        $this->name = $name;
+        $this->age = $age;
+        $this->roll = $roll;
+    }
+}
+
+
+
+?>
